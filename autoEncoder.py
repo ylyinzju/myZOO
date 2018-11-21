f73138f60a9146872dc0b29ee747d35772512aa9
ylyinzju <736498802@qq.com>
2018-11-21 15:33:49 +0800

    Add files via upload

---
+++ autoEncoder.py
@@ -0,0 +1,51 @@
+import os
+import torch
+import modules
+from modules import *
+
+class AutoEncoder:
+    def __init__(self,typename,teacher,student):
+        self.input_size=0
+        for i in teacher:
+            self.input_size = self.input_size + i
+        self.hidden_layer_size = student
+        if typename == 'conv':
+            self.encode_net = getInnerLayer('conv', in_channels = self.input_size, out_channels = self.hidden_layer_size, kernel_size=1)
+            self.decode_net = getInnerLayer('conv', in_channels = self.hidden_layer_size, out_channels = self.input_size, kernel_size=1)
+
+        if typename == 'fc':
+            self.encode_net = getInnerLayer('fc', in_channels = self.input_size, out_channels = self.hidden_layer_size)
+            self.decode_net = getInnerLayer('fc', in_channels = self.hidden_layer_size, out_channels = self.input_size)
+        self.criterion = getLoss('MSE')
+        self.optimizer = getOptimizer('SGD', self.parameters(), lr=0.05, momentum=0.9, weight_decay=0)
+
+    def parameters(self):
+        for pram in [self.encode_net,self.decode_net]:
+            yield pram.parameters()
+    '''
+    def run_step(self,x):
+        distill_feature = self.encode_net(x)
+        reconstruct_feature = self.decode_net(distill_feature)
+        return distill_feature, reconstruct_featrue
+    '''
+    def cal_loss(self, x , target):
+        calcLoss(self.criterion, self.optimizier, x ,target, data_loader)        
+    
+    def train_step(self,data):
+        return self.encode_net(data)
+    
+    def run_step(self,data):
+        return self.decode_net(data)
+    '''
+    def cal_loss(self,x):
+        distill_feature = train_step(x)
+        reconstruct_feature = run_step(distill_feature)
+        loss = self.criterion(x, reconstruct_feature)
+        self.optimizier.zero_grad()
+        loss.backward()
+        self.optimizier.step()
+    '''
+
+
+A = AutoEncoder('conv', [64,64],72)
+print(A)
---
+++ modules.py
@@ -0,0 +1,96 @@
+import os
+import torch
+from torch import nn
+from torch.autograd import Variable
+from torch import optim
+from torch.optim import lr_scheduler
+from torch.autograd import Variable
+import math
+
+
+class innerLayer:
+    def __init__(self,reallayer):
+        print('1')
+        self.reallayer = reallayer
+        self.initialized = False
+        self.databuffer = None 
+        self.reallayer.register_forward_hook(self.obtain_middle_result)
+
+    def __call__(self,data):
+        print('2')
+        if self.initialized == False:
+            self._initialize_weight()
+            self.initialized = True
+        return self.reallayer(data)
+    
+    def _initialize_weight(self):
+        if isinstance(self.reallayer, nn.Conv2d):                                    
+            n = self.reallayer.kernel_size[0] * self.reallayer.kernel_size[1] * self.reallayer.in_channels
+            value = math.sqrt(3. / n)
+            self.reallayer.weight.data.uniform_(-value, value)
+            if self.reallayer.bias is not None:
+                self.reallayer.bias.data.zero_()
+        elif isinstance(self.reallayer, nn.Linear):
+            n = self.reallayer.in_features
+            value = math.sqrt(3. / n)
+            self.reallayer.weight.data.uniform_(-value, value)
+            if self.reallayer.bias is not None:
+                self.reallayer.bias.data.zero_()
+    
+    def get_run_result(self):
+        return self.databuffer
+
+    def obtain_middle_result(self, module,input,output):
+        print('3')
+        self.databuffer = output.data
+
+
+def getInnerLayer(typename,**args):
+    if typename == "conv":
+        if 'in_channels' not in args or 'out_channels' not in args or 'kernel_size' not in args:
+            print("layer initialize parameter error")
+            return
+        return innerLayer(nn.Conv2d(**args))
+    if typename == "fc":
+        if 'in_channels' not in args or 'out_channels' not in args:
+            print("layer initialize parameter error")
+            return 
+        return innerLayer(nn.Linear(**args))
+    
+def getLoss(name,**args):
+    if name=="MSE":
+        return nn.MSELoss(size_average=True)
+    else:
+        print("loss name not supported")
+
+def getOptimizer(name,**args):
+    if name=="SGD":
+        return optim.SGD(**args)
+
+###################not ok
+def calcLoss(criterion, optimizier, input, target, data_loader):
+  #  for i, (data, labels) in enumerate(data_loader):
+  #      if use_cuda:
+  #          data = data.cuda()
+  #      data = Variable(data, valatile=True)
+    loss = criterion(target, x)
+    optimizer.zero_grad()
+    loss.backward()
+    optimizer.step()
+    return loss
+
+def concatFeature(featurelist):
+    resultfeature=None
+    for feature in featurelist:
+        if resultfeature==None:
+            resultfeature=feature
+        else:
+            resultfeature=torch.cat((resultfeature,feature),dim=1)
+    return resultfeature
+
+
+L = getInnerLayer('conv', in_channels=64, out_channels=24, kernel_size=1)
+img = torch.autograd.Variable((torch.arange(32*32*64).view(1,64,32,32)))
+#handle = L.reallayer.register_forward_hook(L.obtain_middle_result)
+L(img)
+print(L.get_run_result())
